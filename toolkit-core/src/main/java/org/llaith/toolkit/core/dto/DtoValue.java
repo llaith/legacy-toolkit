620acdec623cdc11978147c487c64efa5b04f12a
Nos Doughty <cetic.nos@gmail.com>
2013-12-25 23:52:20 +0000

    update copyright information.

--- toolkit-core/src/main/java/org/llaith/toolkit/core/dto/DtoValue.java
+++ toolkit-core/src/main/java/org/llaith/toolkit/core/dto/DtoValue.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+
+  * Copyright (c) 2005 - 2013 Nos Doughty
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+
+ ******************************************************************************/
 package org.llaith.toolkit.core.dto;
 
 import org.llaith.toolkit.common.guard.Guard;
@@ -28,13 +45,6 @@ public class DtoValue<T> implements Dto<DtoValue<T>>, MetadataContainer {
     private T originalValue;
     private T currentValue;
     private T discardValue;
-
-    private transient boolean checkDirty = false;
-    private transient boolean checkStale = false;
-    private transient boolean skipDirty = false;
-    private transient boolean skipStale = false;
-    private transient boolean skipAccept = false;
-    private transient boolean skipCancel = false;
 
     /**
      * Constructs a new DtoValue based on the passed in DtoField definition.
